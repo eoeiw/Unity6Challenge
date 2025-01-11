82e6f7098c797942815fe5720d2734bd66e9d8f7
eoeiw <kyh40324@outlook.com>
2025-01-11 20:48:52 +0900

    점프 애니메이션 설정 완료
    vs code로 스크립트 편집 프로그램 바꾸는 중인데, .NET 뭐시기가 없다고.... 컴 재부팅하는 김에 커밋하고 감
    
    플레이어 점프 시 애니메이션 송출까지 작업

--- Kimchi-Run/Assets/Scripts/Player.cs
+++ Kimchi-Run/Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
     public Rigidbody2D PlayerRigidBody;
 
     private bool isGrounded = true;
+    public Animator PlayerAnimator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,7 +23,8 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)&&isGrounded)
         {
             PlayerRigidBody.AddForceY(JumpForce, ForceMode2D.Impulse);
-            isGrounded = false; 
+            isGrounded = false;
+            PlayerAnimator.SetInteger("State", 1);
         }
 
     }
@@ -30,6 +32,10 @@ public class Player : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Platform"){
+            if (!isGrounded)
+            {
+                PlayerAnimator.SetInteger("State", 2);
+            }
             isGrounded = true;
             // 점프 후 Platform 콜라이더와 Player 콜라이더 충돌 시 isGrounded bool값 true. 
         }
